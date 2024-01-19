af0a579eac42afa89d2ae8abb00d28b7f804a857
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2024-01-19 11:10:30 +0000

    feat: add page navigation links
    1. Add navigation links for the following:
     - Menu page
     - Offers page
     - Orders page
     - Info page

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var name = "no name"
+    @EnvironmentObject var cartManager: CartManager
     
     var body: some View {
-        VStack {
-            TextField("Enter name", text: $name)
-            Text("Hello \(name)")
+        TabView {
+            MenuPage()
+                .tabItem {
+                    Image(systemName: "cup.and.saucer")
+                    Text("Menu")
+                }
+            OffersPage()
+                .tabItem {
+                    Image(systemName: "tag")
+                    Text("Offers")
+                }
+            OrdersPage()
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Order")
+                       
+                }
+                .badge(cartManager.cart.count)
+            InfoPage()
+                .tabItem {
+                    Image(systemName: "info")
+                    Text("Info")
+                }
         }
     }
 }
