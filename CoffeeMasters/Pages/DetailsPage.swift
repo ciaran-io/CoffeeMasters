a582ff6c563f706295ceba99f1cd27242c8e8c81
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2024-01-19 08:31:43 +0000

    feat: add products details page
    1. Setup page to view products detail; includes product name, price & image.

---
+++ CoffeeMasters/Pages/DetailsPage.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftUIView.swift
+//  CoffeeMasters
+//
+//  Created by Ciaran on 18/01/2024.
+//
+
+import SwiftUI
+
+struct DetailsPage: View {
+    
+    @State var quantity = 1
+    
+    var body: some View {
+        ScrollView {
+            Image("DummyImage")
+                .cornerRadius(5)
+                .frame(maxWidth: .infinity, idealHeight: 150, maxHeight: 150)
+                .padding(.top, 32)
+            Text("Product")
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.leading)
+                .padding(24)
+            HStack {
+                Text("$ 4.25 ea")
+                Stepper(value: $quantity, in: 1...10) { }
+            }
+                .frame(maxWidth: .infinity)
+                .padding(30)
+            
+            Text("Subtotal $4.25")
+                .bold()
+                .padding(12)
+            
+            Button("Add \(quantity) to Cart") {
+                //TODO
+            }
+                .padding()
+                .frame(width: 250.0)
+                .background(Color("Alternative2"))
+                .foregroundColor(Color.black)
+                .cornerRadius(25)
+        }
+        //        .navigationTitle(product.name)
+    }
+}
+
+#Preview {
+    DetailsPage()
+}
