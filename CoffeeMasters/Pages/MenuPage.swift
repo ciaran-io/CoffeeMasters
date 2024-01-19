2842b5dbf5acd4bea950db3b532ee581adb953bc
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2024-01-19 08:33:35 +0000

    feat: add product menu page
    1. Displays products and product menus with navigation links to product details.

---
+++ CoffeeMasters/Pages/MenuPage.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIView.swift
+//  CoffeeMasters
+//
+//  Created by Ciaran on 18/01/2024.
+//
+
+import SwiftUI
+
+struct MenuPage: View {
+    
+    @EnvironmentObject var menuManager: MenuManager
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menuManager.menu) { category in
+                    Text(category.name)
+                    
+                    ForEach(category.products) { product in
+                        NavigationLink {
+                            DetailsPage()
+                        } label: {
+                            ProductItem(product: product)
+                        }
+                    }
+                }
+                
+            }.navigationTitle("Products")
+        }
+    }
+}
+
+#Preview {
+    MenuPage().environmentObject(MenuManager())
+}
